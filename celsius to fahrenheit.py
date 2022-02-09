8ca14c2e03c17752f266b58927b19d6618d855bb
Kevin Flyn <96508295+GeeJoe034@users.noreply.github.com>
2022-02-09 21:25:45 +0700

    Create celsius to fahrenheit.py

---
+++ celsius to fahrenheit.py
@@ -0,0 +1,4 @@
+celsius = float(input("ป้อนตัวเลขอุณหภูมิองศาเซลเซียส( ํC) : "))
+fahrenheit = ((9/5) * celsius) + 32
+
+print(f"{celsius:.2f} องศาเซลเซียส( ํC) มีค่าเท่ากับ {fahrenheit:.2f} องศาฟาเรนไฮต์( ํF)")
