21c3e673b8a34e7e7adc61f097bbd3c208095959
Kevin Flyn <96508295+GeeJoe034@users.noreply.github.com>
2022-02-17 21:28:54 +0700

    Create seven.py
    รับค่า 3 ตัว แล้วหาค่า 7

---
+++ seven.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter num 1:"))
+num2=int(input("Enter num 2:"))
+num3=int(input("Enter num 3:"))
+i = 1
+count=0
+while num1 < num3:
+ print(i)
+ if i == 7:
+    count +=1
+ else:
+    break
+ i += 1
