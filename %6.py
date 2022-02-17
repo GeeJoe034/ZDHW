7fc99b2a37859f9e349fa051256035f718eb7c81
Kevin Flyn <96508295+GeeJoe034@users.noreply.github.com>
2022-02-17 21:31:14 +0700

    Create %6.py

---
+++ %6.py
@@ -0,0 +1,11 @@
+i=1
+count=0
+while i < 1001:
+   if( (i % 6) == 0 ):
+       print(i,":divide evenly")
+       count +=1
+       i +=1
+   else:
+       print(i,":undivided")
+       i +=1
+print("cou
