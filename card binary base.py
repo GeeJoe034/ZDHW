8abff76218885ed612032d87ed8e4e4674b97406
Kevin Flyn <96508295+GeeJoe034@users.noreply.github.com>
2022-03-12 23:13:25 +0700

    Update card binary base.py

--- card binary base.py
+++ card binary base.py
@@ -1,4 +1,43 @@
+def show(num):
+    print("***************************************************************")
+    c = 0
+    for n in num:
+        if c % 10 == 0:
+            print('\n: ', end="")
+        if n < 10:
+            print(' ', n, ": ", end="")
+        elif n < 100:
+            print('', n, ": ", end="")
+        else:
+            print(n, ': ', end="")
+        c += 1
+    print("\n***************************************************************")
 
+
+k = 1
+l = 2
+while k < l:
+
+ choice = input("Enter Choice: ")
+ if choice == "1":
+     print("Card Boinary base 1 ")
+     card_number1 = []
+     for i in range(1, 128, 2):
+         card_number1.append(i)
+     show(card_number1)
+ elif choice == "2":
+    print("Card Bainary base 2 ")
+    card_number2 = []
+    i = 2
+    while i < 128:
+        card_number2.append(i)
+        if i % 2 == 0:
+            i += 1
+        else:
+            i += 3
+    show(card_number2)
+ elif choice == "3":
+    print("Card Bainary base 3 ")
     card_number3 = []
     i = 4
     c = 0
